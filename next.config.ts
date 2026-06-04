8aff7abbe35ad64965bfd9222ba478d99a5697a7
seanmd8 <smd8@pdx.edu>
2026-06-04 11:08:03 -0700

    pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    basePath: "algebra-1-jumpstart",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;

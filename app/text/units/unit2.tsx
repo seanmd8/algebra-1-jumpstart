aacd5e6dd1fbbeadc9fc0f27d89e4528e29c15fd
seanmd8 <smd8@pdx.edu>
2026-06-04 11:17:51 -0700

    removed Workflows

--- app/text/units/unit2.tsx
+++ app/text/units/unit2.tsx
@@ -1,7 +1,7 @@
 const unit2Title = `Unit 2: Linear Equations and Systems`;
 const unit2Date = `August 10, 2026 at 4:00`;
 const unit2Text = [
-    `- `,
+    `- Manipulating equations via `,
 ];
 
 export {unit2Title, unit2Date, unit2Text};
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     basePath: "algebra-1-jumpstart",
     output: "export",  // <=== enables static exports
-    reactStrictMode: true,
 };
 
 export default nextConfig;

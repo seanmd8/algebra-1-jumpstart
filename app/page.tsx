280e2a4c0503211e5ff8c4aa3edc02e8e04ec68b
seanmd8 <smd8@pdx.edu>
2026-06-05 12:39:37 -0700

    Add link icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import "./globals.css"
+import {path} from "./path";
 import { homeText1, homeText2 } from "./text/home/text";
 
 export default function Home() {
     return (
         <div className="normal-text-container home-text">
+            <h1 className="page-header">Home</h1>
+            {/*
             <p>{homeText1}</p>
             <br/>
+            */}
             <p>{homeText2}</p>
             <br/>
             <a className="link-style" href="https://www.seattleschools.org/departments/mathematics/secondary-mathematics/high-school-mathematics/">
                 SPS High School Mathematics
+                <img className="link-img" src={`${path}/link_icon.png`}/>
             </a>
             <a className="link-style" href="https://accessim.org/9-12-aga/algebra-1?a=student">
                 Illustrative Mathematics Algebra 1
+                <img className="link-img" src={`${path}/link_icon.png`}/>
             </a>
         </div>
     );
